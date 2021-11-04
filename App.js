32739aadf2f21711c9d349c490722a46cfa0a51e
sieunlee <dltldmsm@gmail.com>
2021-11-04 17:43:40 +0900

    211104

--- App.js
+++ App.js
@@ -6,9 +6,22 @@ import { theme } from './color';
 export default function App() {
 
   const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
-  
+
+  const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = Object.assign({}, toDos, {[Date.now]: {text, work:working}} )
+    setToDos(newToDos);
+    setText("");
+  };
+
   return(
     <View style={styles.container}>
       <StatusBar style="auto"></StatusBar>
@@ -20,7 +33,12 @@ export default function App() {
           <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
         </TouchableOpacity>
       </View>
-      <TextInput placeholder={working ? "Add a To Dos" : "Where do you want to go?"}
+      <TextInput 
+      onSubmitEditing={addToDo}
+      onChangeText={onChangeText}
+      value={text}
+      
+      placeholder={working ? "Add a To Dos" : "Where do you want to go?"}
       style={styles.input} />
     </View>
   );
@@ -47,5 +65,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 30,
     marginTop: 20,
+    fontSize: 20,
   }
 })
