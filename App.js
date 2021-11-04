da9a9a9d6d2ee6dfa88b6bfcad73f4743297f45d
sieunlee <dltldmsm@gmail.com>
2021-11-04 16:13:30 +0900

    211104

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return(
-    <View>
+    <View style={styles.container}>
       <StatusBar style="auto"></StatusBar>
-      <View style={styles.container}>
-        <Text>Work</Text>
-        <Text>Travel</Text>
+      <View style={styles.header}>
+        <Text style={styles.btnText}>Work</Text>
+        <Text style={styles.btnText}>Travel</Text>
       </View>
     </View>
   );
@@ -17,5 +17,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
+  },
+  header:{
+    flexDirection: 'row',
+  },
+  btnText: {
+    color: 'white',
   }
 })
