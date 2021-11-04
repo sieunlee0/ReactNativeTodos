e87b750e5968ca3f25d3890e9f364f395788c643
sieunlee <dltldmsm@gmail.com>
2021-11-04 16:10:08 +0900

    211104

--- App.js
+++ App.js
@@ -5,11 +5,17 @@ export default function App() {
   return(
     <View>
       <StatusBar style="auto"></StatusBar>
-      <View></View>
+      <View style={styles.container}>
+        <Text>Work</Text>
+        <Text>Travel</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  }
 })
